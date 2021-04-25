2e042ef389ab5cf78697df5f68fe712b2cb6037e
AMRUTHA DINESH <59316132+AmruthaDinesh@users.noreply.github.com>
2021-04-26 00:35:03 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,90 @@
+let data = {
+    "Home": {
+        "title": "Hello,I am Amrutha Dinesh",
+        "image": "<img src='https://drive.google.com/file/d/1rf-0tLEnpb-XAITUu0IuYlrbpX7Wh3lJ/view?usp=sharing'>",
+        "description": "I am a second year B.Tech student in the Division of Computer Science at School of Engineering CUSAT.  <br><br>I enjoy attending tech events and testing the limits of technology. I'm working towards bridging the gender gap in technology",
+        "address": "Valiyaparambil House <br>Sree Padmanabha<br>Poonithura P.O Ernakulam<br>Kerala IN 682038",
+        "social": {
+            "Email : ": "<a href='mailto:vpamrutha197@gmail.com.com?Subject=Hello' target='_top'>vpamrutha197@gmail.com</a>",
+            "Phone No : ": "<a href='tel:+919567300299'>+91 9567300299</a>",
+            "LinkedIn : ": "<a href='https://www.linkedin.com/in/amrutha-dinesh-7b0776191/'>AmruthaDinesh</a>",
+            "Twitter : ": "<a href='https://twitter.com/AmruthaDinesh1'>@AmruthaDinesh1</a>",
+            
+        }
+    },
+    "CV": {
+        "title": "To download offline version click <a href='./resume.pdf'>here</a>",
+        "list": {
+            "Education": {
+                "cusat": {
+                    "title": "Cochin University of Science and Technology",
+                    "subtitle": "Bachelor of Technology in Computer Science",
+                    "date": "July 2019 - May 2023",
+                    "description": "<b>CGPA - 9.16 /10</b> <br>Core Courses:- Operating System, Algorithms, Data Structures, Object Oriented Programming Language, Computer Architecture and Organisation, Principles of Programming Languages, Data Mining, Graphics, Automata Languages."
+                }
+            },
+            
+            "Internship": {
+                "Trebuchet": {
+                    "title": "Trebuchet systems",
+                    "subtitle": "Research Intern",
+                    "date": "November 2020 - January 2021",
+                    "description": "The work involved collaborating with senior research team to formulate protocol(DLDAR) to perform dynamic load division between client and server in real-time web based AR."
+                },
+                
+            },
+            "Projects": {
+                "Money Manager": {
+                    "title": "Money Manager",
+                    "subtitle": "Its a simple app to manage your monthly expenses",
+                    "date": "2021",
+                    "description": "Created a discord bot for managing finances and provides features such as monthly pdf exports and graphical UI for category-wise representation..<br><br><b>Status : </b>Work in Progress"
+                },
+                "semantic": {
+                    "title": "Semantic Lab",
+                    
+                    "date": "2021",
+                    "description": "A semantic system that can control the IoT sensors, autoregister and provide internet based command line interface. The system is natively conversational and can be integrated with popular voice agents like Google Home, Amazon Alexa etc.<br><br>"
+                },
+               
+            },
+            "Leadership/Volunteering": {
+                "Robotics": {
+                    "title": "Robotics Club CUSAT",
+                    "subtitle": "Computer vision Engineer",
+                    "date": "2020-present",
+                    "description": "worked on detection and understanding of surroundings by machines."
+                },
+                "TedX Kalamaserry": {
+                    "title": "Web Designer",
+                    "subtitle": "Designed the official website of TedX",
+                    "date": "2021",
+                    "description": "Designed the website along with other members of the team,interacted with sponsors to organise the event."
+                },
+                "Pehia Foundation": {
+                    "title": "Designer and mentor",
+                    "subtitle": "Pehia Summit 2021",
+                    "date": "2021",
+                    "description": "Coordinated and help plan the entire event flow including outreach, technical management, media interaction,participant engaging , selecting themes and activities."
+                },
+                "ISBIS Kochi": {
+                    "title": "Guest Coordinator",
+                    "subtitle": "ISBIS conference 2020",
+                    "date": "2020",
+                    "description": "Made the smooth conduct of the event virtually "
+                },
+                
+            },
+            "Achievments/Certifications": {
+                "coursera1": {
+                    "title": "Programming for Everybody (Getting Started with Python)",
+                    "subtitle": "Coursera",
+                    "date": "2020",
+                    "description": ""
+                }
+               
+            }
+        }
+    }
+    
+}
